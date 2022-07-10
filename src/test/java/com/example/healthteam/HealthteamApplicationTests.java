baddebf29b14d687559d8ff47e211b35444cebaf
JongWoong <78545700+JomMum@users.noreply.github.com>
2022-07-10 20:57:16 +0900

    workflows 수정 + spring 추가
    -workflows 수정
    -spring 프로젝트 추가

---
+++ src/test/java/com/example/healthteam/HealthteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
